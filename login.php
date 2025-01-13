3d667e0152cb7b0afbd1515528ebde366f46e31b
SKTRAIHAN16 <86212236+SKTRAIHAN16@users.noreply.github.com>
2025-01-14 03:36:09 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Login | My Daily Journal</title>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+      crossorigin="anonymous"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css"
+    />
+    <link rel="icon" href="img/logo.png" />
+  </head>
+  <body class="bg-danger-subtle">
+  <div class="container mt-5 pt-5">
+  <div class="row">
+    <div class="col-12 col-sm-8 col-md-6 m-auto">
+      <div class="card border-0 shadow rounded-5">
+        <div class="card-body">
+          <div class="text-center mb-3">
+            <i class="bi bi-person-circle h1 display-4"></i>
+            <p>My Daily Journal</p>
+            <hr />
+          </div>
+          <form action="" method="post">
+            <input
+              type="text"
+              name="user"
+              class="form-control my-4 py-2 rounded-4"
+              placeholder="Username"
+            />
+            <input
+              type="password"
+              name="pass"
+              class="form-control my-4 py-2 rounded-4"
+              placeholder="Password"
+            />
+            <div class="text-center my-3 d-grid">
+              <button class="btn btn-danger rounded-4">Login</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<?php
+//set variable username dan password dummy
+$username = "admin";
+$password = "123456";
+
+//check apakah ada request dengan method POST yang dilakukan
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+    foreach($_POST as $key => $val){
+        echo $key . " : " . $val ."<br>";
+    } 
+
+    
+    if($_POST['user'] == $username AND $_POST['passw'] == $password){
+        echo "Username dan Password Benar";
+    }else{
+        echo "Username dan Password Salah";
+    }
+};
+?>      
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+      crossorigin="anonymous"
+    ></script>
+  </body>
+</html>
